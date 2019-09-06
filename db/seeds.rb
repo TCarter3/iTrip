d2846e3f7a34828fa0092e1a340cb1fcabaf1221
Michael Kroon <kroonology@gmail.com>
2019-09-06 13:12:04 -0600

    improve seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,48 @@
 
 User.create!(email: "test@test.com", password: "password")
-5.times do
-  User.create!(
-    email: Faker::Internet.email,
-    password: "password",
-  )
-end
-
-6.times do
-  Trip.create!(
-    name: Faker::Lorem.sentence(word_count: 3),
-    user_id: rand(1..6)
-  )
-end
-
-6.times do
-  Location.create!(
+Trip.create!(name: Faker::Lorem.sentence(word_count: 3), user_id: 1)
+Location.create!(
     name: Faker::Address.country,
     days: rand(1..10),
-    trip_id: rand(1..5)
-  )
-end
-
-location_records = Location.all
-location_records.each do |record|
-  Address.create!(
+    trip_id: 1
+)
+Address.create!(
     street: Faker::Address.street_name,
     city: Faker::Address.city,
     state: Faker::Address.state,
     zip: Faker::Address.zip,
     location_id: record.id
   )
-end
+# 5.times do
+#   User.create!(
+#     email: Faker::Internet.email,
+#     password: "password",
+#   )
+# end
+
+# 6.times do
+#   Trip.create!(
+#     name: Faker::Lorem.sentence(word_count: 3),
+#     user_id: rand(1..6)
+#   )
+# end
+
+# 6.times do
+#   Location.create!(
+#     name: Faker::Address.count
+#     days: rand(1..10),
+#     trip_id: rand(1..5)
+#   )
+# end
+
+# location_records = Location.all
+# location_records.each do |record|
+#   Address.create!(
+#     street: Faker::Address.street_name,
+#     city: Faker::Address.city,
+#     state: Faker::Address.state,
+#     zip: Faker::Address.zip,
+#     location_id: record.id
+#   )
+# end
 puts "Database successfully seeded."
