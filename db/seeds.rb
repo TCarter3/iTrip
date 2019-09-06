55b0b3817aa9a14a73fbb84b6c35ac08277d9a97
Michael Kroon <kroonology@gmail.com>
2019-09-06 11:57:36 -0600

    create seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "test@test.com", encrypted_password: "password")
+5.times do
+  User.create(
+    email: Faker::Internet.email,
+    encrypted_password: "password"
+  )
+  Trip.create(
+    name: Faker::Lorem.sentence(word_count: 3),
+    user_id: rand(7)
+  )
+  Location.create(
+    name: Faker::Address.country,
+    days: rand(11),
+    trip_id: rand(6)
+  )
+  location_records = Location.all
+  location_records.each do |record|
+    Address.create(
+      street: Faker::Address.street_name,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip,
+      location_id: record.id
+    )
+  end
+end
+puts "Database successfully seeded."
