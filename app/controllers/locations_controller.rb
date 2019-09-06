3e2aa388329b3fe154978ea83b7214bc455dd8ad
Jake Frank <jacob423frank@gmail.com>
2019-09-06 11:19:07 -0600

    locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,56 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
   def index
+    @locations = @trips.locations
   end
-
-  def new
-  end
-
+  
   def show
   end
 
+  def new
+    @location = @trip.locations.new
+  end
+
   def edit
+  end
+
+  def create
+    @location = @trip.locations.new(location_params)
+      
+    if @location.save
+      redirect_to [@trip, @location]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @location.update(location_params)
+      redirect_to [@trip, @location]
+    else 
+      render :new 
+    end
+  end
+
+
+  def destroy
+    @location.destroy
+    redirect_to trip_locations_path
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :days)
   end
 end
