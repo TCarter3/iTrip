25cdeb1f6b32e49bb2f196671fbffd67522155da
Michael Kroon <kroonology@gmail.com>
2019-09-06 12:25:48 -0600

    fine tuning seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: "test@test.com", encrypted_password: "password")
+
+User.create!(email: "test@test.com", password: "password")
 5.times do
-  User.create(
+  User.create!(
     email: Faker::Internet.email,
-    encrypted_password: "password"
+    password: "password",
   )
-  Trip.create(
+end
+
+6.times do
+  Trip.create!(
     name: Faker::Lorem.sentence(word_count: 3),
-    user_id: rand(7)
+    user_id: rand(1..6)
   )
-  Location.create(
+end
+
+6.times do
+  Location.create!(
     name: Faker::Address.country,
-    days: rand(11),
-    trip_id: rand(6)
+    days: rand(1..10),
+    trip_id: rand(1..5)
   )
-  location_records = Location.all
-  location_records.each do |record|
-    Address.create(
-      street: Faker::Address.street_name,
-      city: Faker::Address.city,
-      state: Faker::Address.state,
-      zip: Faker::Address.zip,
-      location_id: record.id
-    )
-  end
+end
+
+location_records = Location.all
+location_records.each do |record|
+  Address.create!(
+    street: Faker::Address.street_name,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip,
+    location_id: record.id
+  )
 end
 puts "Database successfully seeded."
